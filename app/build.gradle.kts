e829e861ccb60f355ed62cca30a1f533132baade
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2026-06-04 09:12:52 -0300

    build: add androidx.fragment.ktx dependency
    - Add `libs.androidx.fragment.ktx` to dependencies in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.constraintlayout)
 }
