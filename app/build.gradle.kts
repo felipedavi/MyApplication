61b99f6a907256e3de12d31748a2ed11b47f3a21
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2026-06-03 21:23:47 -0300

    build: update compileSdk to BAKLAVA
    - Update compileSdk in app/build.gradle.kts to use BAKLAVA instead of hardcoded value 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.myapplication"
     compileSdk {
-        version = release(36) {
+        version = release(BAKLAVA) {
             minorApiLevel = 1
         }
     }
